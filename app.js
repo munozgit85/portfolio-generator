b44ef41eb03e9bdfb809e04c1603760526e94bd3
munozgit85 <107218022+munozgit85@users.noreply.github.com>
2022-09-07 13:44:04 -0500

    feature branch

--- app.js
+++ app.js
@@ -1,15 +1,37 @@
+const fs = require('fs');
+const generatePage = require('./src/page-template');
+
 const profileDataArgs = process.argv.slice(2);
 
-const printProfileData = profileDataArr => {
-  // This...
-  for (let i = 0; i < profileDataArr.length; i += 1) {
-    console.log(profileDataArr[i]);
-  }
+console.log(profileDataArgs);
 
-  console.log('================');
+const [name, github] = profileDataArgs;
+
+console.log(name, github);
+
+const pageHTML = generatePage(name, github);
+
+fs.writeFile('./index.html', pageHTML, err => {
+  if (err) throw err;
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
+
+//same as const name = profileDataArgs[0];
+//const github = profileDataArgs[1];
+
+
+
+//const printProfileData = profileDataArr => {
+  // This...
+  //for (let i = 0; i < profileDataArr.length; i += 1) {
+    //console.log(profileDataArr[i]);
+  //}
+
+  //console.log('================');
 
   // Is the same as this...
-  profileDataArr.forEach(profileItem => console.log(profileItem));
-};
+  //profileDataArr.forEach(profileItem => console.log(profileItem));
+//};
 
-printProfileData(profileDataArgs);
+//printProfileData(profileDataArgs);
